4edc988ffd4a731323961a7ee28b519fde2c2aeb
Arch <33100644+mttorres@users.noreply.github.com>
2021-07-15 15:07:41 -0300

    Add github profile to SideBar and using "as" for box on

--- src/components/ProfileSideBar/index.js
+++ src/components/ProfileSideBar/index.js
@@ -1,9 +1,20 @@
 import Box from  '../Box';
+import { AlurakutProfileSidebarMenuDefault} from '../../lib/AlurakutCommons';
 
 function ProfileSideBar(prop){
-    return (<Box>
+    return (
+      <Box as="aside">
         <img src={`https://github.com/${prop.githubUser}.png`}/>
-      </Box>); 
+        <hr />
+        <p>
+          <a className="boxLink" href={`https://github.com/${prop.githubUser}`}>
+            @{prop.githubUser}
+          </a>
+        </p>
+        <hr />
+        <AlurakutProfileSidebarMenuDefault />
+      </Box>
+      ); 
   }
 
 export default ProfileSideBar;
