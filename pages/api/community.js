9b84de0b5ca9596f48dbfae70942acd862a5b4f9
Arch <33100644+mttorres@users.noreply.github.com>
2021-07-19 02:45:46 -0300

    Undo last commits

--- pages/api/community.js
+++ pages/api/community.js
@@ -4,39 +4,34 @@ import {tokenP} from '../../config/config';
 export default async function requestInterceptor(request, response){
     const bodyData = request.body;
     const client = new SiteClient(tokenP);
+    //console.log(request);
     // poderia ser put né...
-    try{
-        if(request.method == 'POST'){
-            const path = await client.createUploadPath('./pages/local-image-cache/temp-image.png');
-            const upload = await client.uploads.create({
-                path,
-    
-                author: bodyData.userName,
-                copyright: "AluraKut"
-            });
-            // Enviar comunidade
-            const record = await client.items.create({
-                itemType: "972360",
-                title: bodyData.title,
-                link: null, // melhorar (não temos página de comunidades)
-                image: null, // melhorar (ele não consegue mandar a imagem reclama que deve ser um object, e quando coloca object reclama que deveria ser uma string!)
-                imageUrl : upload.url,
-                creatorSlug: bodyData.userName
-            });
-            // NÃO É PRINTADO NO FRONT NEM TRAFEGADO POR LÁ!
-            response.json({
-                record: record
-            });
-            return;
-        }
-    
-        response.status(405).json({
-            message: 'Invalid Method Request'
-        })        
-    } catch(e){
-        response.status(500).json({
-            message: e
+
+    if(request.method == 'POST'){
+        const path = await client.createUploadPath('./pages/local-image-cache/temp-image.png');
+        const upload = await client.uploads.create({
+            path,
+
+            author: bodyData.userName,
+            copyright: "AluraKut"
         });
+        // Enviar comunidade
+        const record = await client.items.create({
+            itemType: "972360",
+            title: bodyData.title,
+            link: null, // melhorar (não temos página de comunidades)
+            image: null, // melhorar (ele não consegue mandar a imagem reclama que deve ser um object, e quando coloca object reclama que deveria ser uma string!)
+            imageUrl : upload.url,
+            creatorSlug: bodyData.userName
+        });
+        // NÃO É PRINTADO NO FRONT NEM TRAFEGADO POR LÁ!
+        response.json({
+            record: record
+        });
+        return;
     }
 
+    response.status(405).json({
+        message: 'Invalid Method Request'
+    })
 }
--- pages/api/fileImage.js
+++ pages/api/fileImage.js
@@ -1,33 +1,25 @@
 import fs from 'fs';
 
 export default async function requestInterceptor(request, response){
-    try{
-    
-        if(request.method == 'PUT'){
-            //melhorar depois
-            fs.writeFile(`./pages/local-image-cache/temp-image.png`, Buffer.from(request.body.replace(/^data:image\/\w+;base64,/, ""),'base64'),
-            (err) => {
-                    if(err){
-                        console.log('Error!');
-                        response.status(500).json({
-                            message:  err
-                        })
-                    }
-            });
-            console.log('image saved into cache');
-            response.status(201).json({
-                message: 'image saved into cache'
-            })
-            return;
-    
-        }
-        response.status(405).json({
-            message: 'Invalid Method Request'
+    if(request.method == 'PUT'){
+        //melhorar depois
+        fs.writeFile(`./pages/local-image-cache/temp-image.png`, Buffer.from(request.body.replace(/^data:image\/\w+;base64,/, ""),'base64'),
+        (err) => {
+                if(err){
+                    console.log('Error!');
+                    console.log(err);
+                    throw err;
+                }
+        });
+        console.log('image saved into cache');
+        response.status(201).json({
+            message: 'image saved into cache'
         })
         return;
-    } catch (e){
-        response.status(500).json({
-            message: e
-        })
+
     }
+    response.status(405).json({
+        message: 'Invalid Method Request'
+    })
+    return;
 }
