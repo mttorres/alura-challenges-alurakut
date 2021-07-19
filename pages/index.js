1bef06dc9ceeab5668fed0e67357749de9758542
Arch <33100644+mttorres@users.noreply.github.com>
2021-07-19 01:53:43 -0300

    Cleaning main index

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,27 @@ import ProfileSideBar from  '../src/components/ProfileSideBar';
 import WelcomeArea from '../src/components/WelcomeArea';
 import RelationsArea from  '../src/components/RelationsArea';
 import requestFollowers from '../src/requesters/followers';
-import requestCommu from '../src/requesters/community';
+import {requestAllCommu} from '../src/requesters/community';
 
 //XMLHttpRequest não serve é request para o server local? NAO! é para binários e etc
 
 export default function Home() {
   const userName = "mttorres";
   const [followers, setFollowers] = React.useState([]);
-  requestFollowers(userName,setFollowers);
+  React.useEffect(() => {
+    requestFollowers(userName)
+    .then(data => setFollowers(data));
+  }, []);
+
   const galera = [{"id": "1", "login": 'juunegreiros'}, {"id": "2", "login": "omariosouto"}, 
   {"id": "3", "login": "peas"}, {"id": "5", "login": "rafaballerini"}, {"id": "6", "login": "marcobrunodev"},
   {"id": "7", "login": "felipefialho"}, ]
-  const [comunidades, setComunidades] = React.useState([]);
-  requestCommu(setComunidades);
+  
+  const [comunidades, setCommu] = React.useState([]);
+  React.useEffect(() => {
+    requestAllCommu()
+    .then(data => setCommu(data));
+  },[]);
 
   return (
   <> 
@@ -26,7 +34,7 @@ export default function Home() {
     <div className="profileArea" style={{gridArea: 'profileArea'}}>
         <ProfileSideBar githubUser={userName} />
     </div>    
-    <WelcomeArea userStatVideos={999} userStatconfiavel={3} userStatLegal={3} community={comunidades} seterCommunity = {setComunidades} />
+    <WelcomeArea userName={userName} userStatVideos={999} userStatconfiavel={3} userStatLegal={3} community={comunidades} seterCommunity = {setCommu} />
     <RelationsArea galera ={galera} followers={followers} community={comunidades} />
   </MainGrid>
   </> );
