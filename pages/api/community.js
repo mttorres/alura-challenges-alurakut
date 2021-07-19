c81f9a9656cb079a4bdaafe0acfc12034abc610c
Arch <33100644+mttorres@users.noreply.github.com>
2021-07-19 02:31:00 -0300

    Adding try/catch as a attempt of finding the error

--- pages/api/community.js
+++ pages/api/community.js
@@ -4,33 +4,39 @@ import {tokenP} from '../../config/config';
 export default async function requestInterceptor(request, response){
     const bodyData = request.body;
     const client = new SiteClient(tokenP);
-    console.log(request);
     // poderia ser put né...
-    if(request.method == 'POST'){
-        const path = await client.createUploadPath('./pages/local-image-cache/temp-image.png');
-        const upload = await client.uploads.create({
-            path,
-
-            author: bodyData.userName,
-            copyright: "AluraKut"
-        });
-        // Enviar comunidade
-        const record = await client.items.create({
-            itemType: "972360",
-            title: bodyData.title,
-            link: null, // melhorar (não temos página de comunidades)
-            image: null, // melhorar (ele não consegue mandar a imagem reclama que deve ser um object, e quando coloca object reclama que deveria ser uma string!)
-            imageUrl : upload.url,
-            creatorSlug: bodyData.userName
-        });
-        // NÃO É PRINTADO NO FRONT NEM TRAFEGADO POR LÁ!
+    try{
+        if(request.method == 'POST'){
+            const path = await client.createUploadPath('./pages/local-image-cache/temp-image.png');
+            const upload = await client.uploads.create({
+                path,
+    
+                author: bodyData.userName,
+                copyright: "AluraKut"
+            });
+            // Enviar comunidade
+            const record = await client.items.create({
+                itemType: "972360",
+                title: bodyData.title,
+                link: null, // melhorar (não temos página de comunidades)
+                image: null, // melhorar (ele não consegue mandar a imagem reclama que deve ser um object, e quando coloca object reclama que deveria ser uma string!)
+                imageUrl : upload.url,
+                creatorSlug: bodyData.userName
+            });
+            // NÃO É PRINTADO NO FRONT NEM TRAFEGADO POR LÁ!
+            response.json({
+                record: record
+            });
+            return;
+        }
+    
+        response.status(405).json({
+            message: 'Invalid Method Request'
+        })        
+    } catch(e){
         response.json({
-            record: record
+            message: e
         });
-        return;
     }
 
-    response.status(405).json({
-        message: 'Invalid Method Request'
-    })
 }
